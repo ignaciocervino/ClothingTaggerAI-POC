ca2ae70f2b12b5e6c1b2a973ba48a5c2f9cf7d0b
Ignacio Cervino <ignaciocervino@gmail.com>
2025-03-03 13:45:14 -0300

    Initial Commit

---
+++ ClothTaggerAI/ClothTaggerAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClothTaggerAIApp.swift
+//  ClothTaggerAI
+//
+//  Created by Ignacio Cervino on 03/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ClothTaggerAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
